ba58cfe78a0eac95a58a09a183eb0e1de10641d2
German <ghacke@gmail.com>
2019-02-08 22:39:23 -0300

    Varaible de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://user:5sd4fg5a454dsd@ds125225.mlab.com:25225/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
